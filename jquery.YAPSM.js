443c5a890436e39e4253247964f30d336ae1c063
Kjel Delaey <kjel_delaey@hotmail.com>
2011-04-09 11:21:52 +0200

    Set entropy map

--- jquery.YAPSM.js
+++ jquery.YAPSM.js
@@ -9,6 +9,7 @@
 
     $.yapsm = function(options) {
         this.settings = $.extend({}, $.yapsm.defaults, options);
+        this.entropyMap = $.extend({}, $.yapsm.entropyMap);
         this.field = $(this);
         this.init();
     };
@@ -22,6 +23,37 @@
             goodClass: 'good',
             strongClass:'strong',
             veryStrongClass: 'very-strong'
+        },
+
+        entropyMap: {
+            decimal: {
+                count: 10,
+                entropy: 3.32
+            },
+            hexadecimal: {
+                count: 16,
+                entropy: 4.00
+            },
+            mixedCaseLettersOnly: {
+                count: 26,
+                entropy: 4.70
+            },
+            mixedCaseLettersAndNumbers: {
+                count: 36,
+                entropy: 5.17
+            },
+            caseSensitiveLettersOnly: {
+                count: 52,
+                entropy: 5.70
+            },
+            caseSensitiveLettersAndNumbers: {
+                count: 62,
+                entropy: 5.95
+            },
+            otherCharacters: {
+                count: 94,
+                entropy: 6.55
+            }
         },
 
         prototype: {
