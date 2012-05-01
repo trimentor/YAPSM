962867ddd59e2aabd2df2a36c0305665a0bd0426
Kjel Delaey <kjel_delaey@hotmail.com>
2012-05-01 18:20:12 +0200

    ran code against JSLint

--- jquery.YAPSM.js
+++ jquery.YAPSM.js
@@ -4,21 +4,21 @@
  * Copyright 2011, Kjel Delaey (https://github.com/trimentor/YAPSM)
  * https://github.com/trimentor/YAPSM/blob/master/LICENSE
  */
-(function($) {
+(function ($) {
     $.extend($.fn, {
-        yapsm: function(options) {
-            var passwordStrengthMeter = new $.yapsm(options);
-            var calculatePasswordComplexity = function() {
+        yapsm: function (options) {
+            var passwordStrengthMeter, calculatePasswordComplexity;
+            passwordStrengthMeter = new $.yapsm(options);
+
+            calculatePasswordComplexity = function () {
                 this.complexity = passwordStrengthMeter.strength(this.value);
             };
-
             $(this).keyup(calculatePasswordComplexity);
-
             return this;
         }
     });
 
-    $.yapsm = function(options) {
+    $.yapsm = function (options) {
         this.settings = $.extend({}, $.yapsm.defaults, options);
         this.entropyMap = $.extend({}, $.yapsm.entropyMap);
         this.field = $(this);
@@ -32,7 +32,7 @@
             weakClass: 'weak',
             fairClass: 'fair',
             goodClass: 'good',
-            strongClass:'strong',
+            strongClass: 'strong',
             veryStrongClass: 'very-strong',
             commonWordClass: 'dictionary-word'
         },
@@ -63,134 +63,117 @@
                 entropy: 5.95
             },
             otherCharacters: {
-                //count: 94,
                 count: 1,
                 entropy: 6.55
             }
         },
 
         prototype: {
-            init: function() {
+            init: function () {
                 this.commonWords(this.settings.dictionary);
             },
 
-            commonWords: function(dictionary) {
+            commonWords: function (dictionary) {
                 var commonList;
-                var dictionaryType = typeof(dictionary);
 
-                if (dictionaryType == 'function') {
+                if (typeof dictionary === "function") {
                     commonList = dictionary();
-                }
-                else if (dictionaryType == 'object') {
+                } else if (typeof dictionary === "object") {
                     commonList = dictionary;
+                } else {
+                    commonList = [];
                 }
-                else {
-                    commonList = new Array();
-                }
-
                 this.dictionary = commonList;
             },
 
-            strength: function(password) {
-                var dictionaryWord = this.commonWord(password);
-                var bits = !dictionaryWord ? this.passwordStrength(password) : null;
-
+            strength: function (password) {
+                var dictionaryWord, bits;
+                dictionaryWord = this.commonWord(password);
+                bits = !dictionaryWord ? this.passwordStrength(password) : null;
                 return this.strengthClass(bits);
             },
 
-            charSetEntropy: function(charSet) {
+            charSetEntropy: function (charSet) {
                 return Math.log(charSet) / Math.log(2);
             },
 
-            passwordStrength: function(password) {
+            passwordStrength: function (password) {
                 var charSet = this.numberOfPossibleSymbols(password);
-
                 return this.charSetEntropy(charSet) * password.length;
             },
 
-            commonWord: function(password) {
-                var lPassword = password.toLowerCase();
-                var dictionaryWord = false;
+            commonWord: function (password) {
+                var lPassword, dictionaryWord, i;
+                lPassword = password.toLowerCase();
+                dictionaryWord = false;
 
-                for (var i=0; i<this.dictionary.length; i++) {
-                    var commonWord = this.dictionary[i].toLowerCase();
-
-                    if (lPassword == commonWord) {
+                for (i = 0; i < this.dictionary.length; i += 1) {
+                    if (lPassword === this.dictionary[i].toLowerCase()) {
                         dictionaryWord = true;
                         break;
                     }
                 }
-
                 return dictionaryWord;
             },
 
-            numberOfPossibleSymbols: function(password) {
-                var character;
-                var score = 0;
-                var inclDecimal, inclLowerCase, inclUpCase, inclOther;
+            numberOfPossibleSymbols: function (password) {
+                var character, score, inclDecimal, inclLowerCase, inclUpCase, inclOther, i;
+                score = 0;
 
-                for (var i=0; i<password.length; i++) {
+                for (i = 0; i < password.length; i += 1) {
                     character = password.charAt(i);
 
-                    if (typeof(inclDecimal) == 'undefined' && /[0-9]/.test(character)) {
+                    if (typeof inclDecimal === "undefined" && /[0-9]/.test(character)) {
                         inclDecimal = true;
                         score += this.entropyMap.decimal.count;
                     }
-                    if (typeof(inclUpCase) == 'undefined' && /[A-Z]/.test(character)) {
+                    if (typeof inclUpCase === "undefined" && /[A-Z]/.test(character)) {
                         inclUpCase = true;
                         score += this.entropyMap.mixedCaseLettersOnly.count;
                     }
-                    if (typeof(inclLowerCase) == 'undefined' && /[a-z]/.test(character)) {
+                    if (typeof inclLowerCase === "undefined" && /[a-z]/.test(character)) {
                         inclLowerCase = true;
                         score += this.entropyMap.mixedCaseLettersOnly.count;
                     }
-                    if (typeof(inclOther) == 'undefined' && "`~-_=+[{]}\\|;:'\",<.>/?!@#$%^&*()".indexOf(character) >= 0) {
+                    if (typeof inclOther === "undefined" && "`~-_=+[{]}\\|;:'\",<.>/?!@#$%^&*()".indexOf(character) >= 0) {
                         inclOther = true;
                         score += this.entropyMap.otherCharacters.count;
                     }
                 }
-
                 return score;
             },
 
-            strengthClass: function(bits) {
+            strengthClass: function (bits) {
                 var cssClass;
 
-                if (bits == null) {
-                    cssClass = this.settings.commonWordClass
-                }
-                else {
+                if (bits === null) {
+                    cssClass = this.settings.commonWordClass;
+                } else {
                     if (bits >= 28 && bits <= 35) {
                         cssClass = this.settings.fairClass;
-                    }
-                    else if (bits >= 36 && bits <= 59) {
+                    } else if (bits >= 36 && bits <= 59) {
                         cssClass = this.settings.goodClass;
-                    }
-                    else if (bits >= 60 && bits <= 127) {
+                    } else if (bits >= 60 && bits <= 127) {
                         cssClass = this.settings.strongClass;
-                    }
-                    else if (bits >= 128) {
+                    } else if (bits >= 128) {
                         cssClass = this.settings.veryStrongClass;
-                    }
-                    else {
+                    } else {
                         cssClass = this.settings.weakClass;
                     }
                 }
-
                 return cssClass;
             },
 
-            passwordGuesses: function(bits, feelingLucky) {
-                var nBits = !!feelingLucky ? bits-1 : bits;
-
+            passwordGuesses: function (bits, feelingLucky) {
+                var nBits = !!feelingLucky ? bits - 1 : bits;
                 return Math.pow(2, nBits);
             },
 
-            timeToGuess: function(passwordGuesses) {
-                var averagePassPerSec = Math.pow(10, 9)*3;
-                var seconds = Math.floor(passwordGuesses)/averagePassPerSec;
-
-                return [seconds, seconds*60, seconds*3600];
+            timeToGuess: function (passwordGuesses) {
+                var averagePassPerSec, seconds;
+                averagePassPerSec = Math.pow(10, 9) * 3;
+                seconds = Math.floor(passwordGuesses) / averagePassPerSec;
+                return [seconds, seconds * 60, seconds * 3600];
             }
         }
     });
